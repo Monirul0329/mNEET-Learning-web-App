e02bf0aee17bc09b821920431fcf15bc00e7f6da
Monirul0329 <monirulgen45@gmail.com>
2026-06-13 15:56:03 +0530

    Refactor service worker for caching and fetching

--- sw.js
+++ sw.js
@@ -1,5 +1,34 @@
-self.addEventListener('install', e => {
-    e.waitUntil(caches.open('mneet-v1').then(cache => cache.addAll(['/', '/index.html', '/dashboard.html'])));
-});
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(res => res || fetch(e.request))));
+const CACHE_NAME = 'mneet-app-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './dashboard.html',
+  './study_zone.html',
+  './study.html',
+  './ncert_reader.html',
+  './quiz.html',
+  './error_book.html',
+  './mstore.html',
+  './special_hub.html',
+  './support.html',
+  './manifest.json'
+];
 
+// Installation Cycle Hook Cache Data Loops
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('mNEET Engine: Operational service workers assets caching success!');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Intercept Request Telemetry for Instant Offline Returns Logic
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
